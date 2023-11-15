1514bf4b0ffcceb9d7c440fb39b2e63fa2d0f0cc
Jacob <jfeathers.03@gamil.com>
2023-11-15 13:32:48 -0700

    Script Re-Organization
    I separated the movement and ladder climb into different scripts. I also moved some common used functions and variables into the player manager for ease of use. Some functions and variables where also re-named. As of right now the switch case in the player manager script does not do anything but will be used to restrict certain scripts when conditions are met

--- Clamber/Assets/Scripts/Player Scripts/PlayerManager.cs
+++ Clamber/Assets/Scripts/Player Scripts/PlayerManager.cs
@@ -1,9 +1,101 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
-using UnityEngine.Animations;
 
 public class PlayerManager : MonoBehaviour
 {
-    
+    public static PlayerManager Instance;
+
+    [HideInInspector]
+    public PlayerState state;
+
+    [HideInInspector]
+    public static Action<PlayerState> OnStateChange;
+
+    [HideInInspector]
+    public Rigidbody2D R_Body;
+
+    [HideInInspector]
+    public int ignoreLayers;
+
+    [HideInInspector]
+    public bool jumpException;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        R_Body = GetComponent<Rigidbody2D>();
+
+        UpdatePlayerState(PlayerState.BaseState);
+
+        ignoreLayers = (1 << 3) + (1 << 6);
+
+        jumpException = false;
+    }
+
+    public void UpdatePlayerState(PlayerState newState)
+    {
+        state = newState;
+
+        switch (newState)
+        {
+            case PlayerState.BaseState:
+                break;
+
+            case PlayerState.Climbing:
+                break;
+
+            case PlayerState.Grappling:
+                break;
+
+            default:
+                break;
+        }
+
+        OnStateChange?.Invoke(newState);
+    }
+
+    public enum PlayerState
+    {
+        BaseState,
+        Climbing,
+        Grappling
+    }
+
+    public Vector2 MousePosition()
+    {
+        Vector2 mousePos = Input.mousePosition;
+        return Camera.main.ScreenToWorldPoint(mousePos);
+    }
+
+    public Vector2 M_Input()
+    {
+        return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    public Vector3 RelativeDirection(Vector2 origin, Vector2 destination)
+    {
+        return (destination - origin).normalized;
+    }
+
+    // Ground check using raycasts
+    public bool Grounded()
+    {
+        bool isGrounded = false;
+        Ray2D leftRay = new Ray2D(new Vector2(transform.position.x - 0.4f, transform.position.y), transform.TransformDirection(0, -1, 0) * 0.6f);
+        Ray2D centerRay = new Ray2D(new Vector2(transform.position.x, transform.position.y), transform.TransformDirection(0, -1, 0) * 0.6f);
+        Ray2D rightRay = new Ray2D(new Vector2(transform.position.x + 0.4f, transform.position.y), transform.TransformDirection(0, -1, 0) * 0.6f);
+
+        // If any raycasts detect a surface grounded is true
+        if (Physics2D.Raycast(leftRay.origin, Vector2.down, 0.6f, ~ignoreLayers) || Physics2D.Raycast(centerRay.origin, Vector2.down, 0.6f, ~ignoreLayers) || Physics2D.Raycast(rightRay.origin, Vector2.down, 0.6f, ~ignoreLayers))
+        {
+            isGrounded = true;
+        }
+        else { isGrounded = false; }
+
+        return isGrounded;
+    }
 }
--- Clamber/Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Clamber/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -1,94 +1,27 @@
-using System;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody2D rb;
-
-    GameObject ladderObject;
+    PlayerManager Instance;
 
     //Player Movement Variables
     public float m_Speed;
-    public float climb_Speed;
     public float jumpForce;
-    float inputX;
-    float inputY;
-    float lastInputX;
     float timer;
-
-    // Delay Jump Variables
-    private bool delayJump = false;
-    private float delayDuration = 0.4f; // Delay duration in seconds
-    private float delayEndTime;
-
-    int playerLayer;
-    int ladderLayer;
-    int raycastIgnoreLayers;
-
-    bool mounted;
-    bool onLadder;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        mounted = false;
+        Instance = PlayerManager.Instance;
         timer = 0;
     }
 
     private void Update()
     {
-        // Getting the layers in binary format for layermask
-        playerLayer = 1 << 3;
-        ladderLayer = 1 << 6;
-        raycastIgnoreLayers = playerLayer + ladderLayer;
-        // Get key inputs from the unity input system
-        inputX = Input.GetAxisRaw("Horizontal");
-        inputY = Input.GetAxisRaw("Vertical");
-
-        // Call jump function if player is grounded and key is pressed
-        if (Grounded() || LadderMounted())
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                if (onLadder)
-                {
-                    // Jump off the ladder
-                    mounted = false;
-                    rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-                    rb.velocity = Vector2.up * jumpForce;
-                }
-                else
-                {
-                    Jump();
-                    mounted = false;
-                }
-            }
-        }
-
-        // QOL Update: If a player is mounted to a ladder and drops with d or a, they still have 0.3 of a second to hit space to jump
-        //             This is to prevent the player from hitting space and (a/d) at the same time and not jumping
-
-        if (LadderTimer() && onLadder)
+        if (Instance.Grounded() || Instance.jumpException)
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 Jump();
-            }
-        }
-
-        // If mounted on a ladder climb or descend ladder
-        if (LadderMounted())
-        {
-            transform.position = new Vector2(ladderObject.transform.position.x, transform.position.y);
-            if (inputY > 0 || inputY < 0)
-            {
-                transform.position += new Vector3(0, inputY * climb_Speed * Time.deltaTime, 0);
-            }
-            else
-            {
-                transform.position = transform.position;
             }
         }
     }
@@ -96,7 +29,7 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         // If movement keys pressed move character
-        if (inputX > 0 || inputX < 0)
+        if (Instance.M_Input().x > 0 || Instance.M_Input().x < 0)
         {
             MovePlayer();
         }
@@ -105,14 +38,14 @@ public class PlayerMovement : MonoBehaviour
     void MovePlayer()
     {
         // If the player is in the air, apply a constant force in the current direction
-        if (!Grounded())
+        if (!Instance.Grounded())
         {
-            rb.velocity = new Vector2(inputX * m_Speed, rb.velocity.y);
+            Instance.R_Body.velocity = new Vector2(Instance.M_Input().x * m_Speed, Instance.R_Body.velocity.y);
         }
         else
         {
             // If the player is on the ground, move based on user input
-            rb.position += new Vector2(inputX * m_Speed * Time.deltaTime, 0f);
+            Instance.R_Body.position += new Vector2(Instance.M_Input().x * m_Speed * Time.deltaTime, 0f);
 
             float moveSoundTimer = FindObjectOfType<AudioManager>().SoundLength("Grass Walking");
             if (timer <= 0)
@@ -123,97 +56,12 @@ public class PlayerMovement : MonoBehaviour
             if (timer > 0)
             {
                 timer -= Time.deltaTime;
-                Debug.Log(timer);
             }
         }
     }
 
-    void Jump()
+    public void Jump()
     {
-        rb.velocity = Vector2.up * jumpForce;
-    }
-
-    // Ground check using raycasts
-    bool Grounded()
-    {
-        bool isGrounded = false;
-        Ray2D leftRay = new Ray2D(new Vector2(transform.position.x - 0.4f, transform.position.y), transform.TransformDirection(0, -1, 0) * 0.6f);
-        Ray2D centerRay = new Ray2D(new Vector2(transform.position.x, transform.position.y), transform.TransformDirection(0, -1, 0) * 0.6f);
-        Ray2D rightRay = new Ray2D(new Vector2(transform.position.x + 0.4f, transform.position.y), transform.TransformDirection(0, -1, 0) * 0.6f);
-
-        // If any raycasts detect a surface grounded is true
-        if (Physics2D.Raycast(leftRay.origin, Vector2.down, 0.6f, ~raycastIgnoreLayers) || Physics2D.Raycast(centerRay.origin, Vector2.down, 0.6f, ~raycastIgnoreLayers) || Physics2D.Raycast(rightRay.origin, Vector2.down, 0.6f, ~raycastIgnoreLayers) || LadderMounted())
-        {
-            isGrounded = true;
-        }
-        else { isGrounded = false; }
-
-        return isGrounded;
-    }
-
-    // Mount / Dismount the player on the ladder
-    bool LadderMounted()
-    {
-        if (onLadder)
-        {
-            if (inputY < 0 || inputY > 0)
-            {
-                mounted = true;
-                rb.constraints = RigidbodyConstraints2D.FreezeAll;
-            }
-            if (inputX < 0 && inputY == 0 || inputX > 0 && inputY == 0)
-            {
-                mounted = false;
-                rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-            }
-        }
-        else
-        {
-            mounted = false;
-            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-        }
-
-        return mounted;
-    }
-
-    // Delay jump function
-    bool LadderTimer()
-    {
-        if (LadderMounted())
-        {
-            delayJump = true;
-
-            // Set the end time for the delay
-            delayEndTime = Time.time + delayDuration;
-        }
-
-        if (delayJump && Time.time >= delayEndTime)
-        {
-            delayJump = false;
-        }
-        return delayJump;
-    }
-
-
-
-
-    // Trigger detects when player is touching a climbable surface
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.layer == 6)
-        {
-            onLadder = true;
-            ladderObject = collision.gameObject;
-        }
-    }
-
-    // Trigger exit detects when player is no longer touching a climbable surface
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.layer == 6)
-        {
-            onLadder = false;
-            ladderObject = null;
-        }
+        Instance.R_Body.velocity = Vector2.up * jumpForce;
     }
 }
